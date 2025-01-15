9381c4aedb144834caf57613d8d23fb6a63b50b2
Bee-code-tech <babawaleolatunji64@gmail.com>
2025-01-15 23:04:24 +0100

    abu d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Image from "next/image";
 import GridPattern from "@/components/ui/grid-pattern";
 import { StarknetProvider } from "@/components/StarknetProvider";
 
